98587fe878ab06784fccce22e07ec82898d67a73
weihsin2002 <35787286+weihsin2002@users.noreply.github.com>
2018-06-25 10:48:22 -0700

    Start Buildling Earth Plate

--- QiMen/src/org/dao/qimen/model/EarthPlate.java
+++ QiMen/src/org/dao/qimen/model/EarthPlate.java
@@ -3,6 +3,6 @@ package org.dao.qimen.model;
 import org.dao.core.Palaces;
 
 public class EarthPlate {
-	
-
+	private Palace KanPalace;
+	private Palace GenPalace;
 }
--- QiMen/src/org/dao/qimen/model/Palace.java
+++ QiMen/src/org/dao/qimen/model/Palace.java
@@ -9,10 +9,24 @@ public class Palace {
 	
 	Palaces palaces;
 	HeavenlyStems heavenlySteams;
-	EarthlyBranches earthlyBranches;
+	EarthlyBranches earthlyBranch1;
+	EarthlyBranches earthlyBranch2;
 	
-	Palace (int order) {
+	Palace (int order, int ju) {
 		this.order = order;
 		this.palaces = Palaces.fromOrder(order);
+		this.earthlyBranch1 = this.palaces.earthlyBranch1();
+		this.earthlyBranch2 = this.palaces.earthlyBranch2();
+		
+		this.heavenlySteams = HeavenlyStems.heavenlyStems(getHeavenlyStemOrder(ju));
 	}
+	
+	private int getHeavenlyStemOrder(int ju) {
+		ju = Math.abs(ju);
+		if(ju > 0) {
+			return (10+this.order-ju)%9==0 ? 9 : (10+this.order-ju)%9;
+		}else{
+			return (10-this.order+ju)%9==0 ? 9 : (10-this.order+ju)%9;
+		}
+	}	
 }
